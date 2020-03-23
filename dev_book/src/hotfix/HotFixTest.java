e52b83bb24cd98918fe57ed98d61e1221fee5318
chanyang1142 <chanyang1142@gmail.com>
2020-03-23 15:43:46 +0900

    hotfix브렌치로 파일 추가함

---
+++ dev_book/src/hotfix/HotFixTest.java
@@ -0,0 +1,8 @@
+package hotfix;
+
+public class HotFixTest {
+	public static void main(String[] args) {
+		//프로젝트명 ->오른쪽-remote-switch-new branch -hotfix 
+		System.out.println("hotfix 브렌치에서 추가한 소스임");
+	}
+}
