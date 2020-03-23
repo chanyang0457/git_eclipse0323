f006a38779a842f13e63e2df7da3d9b91f2110e2
chanyang1142 <chanyang1142@gmail.com>
2020-03-23 16:33:13 +0900

    ㅇㅇ

--- dev_book/src/hotfix/HotFixTest2.java
+++ dev_book/src/hotfix/HotFixTest2.java
@@ -3,6 +3,6 @@ package hotfix;
 public class HotFixTest2 {
 	public static void main(String[] args) {
 		//프로젝트명 ->오른쪽-remote-switch-new branch -hotfix 
-		System.out.println("hotfix 브렌치에서 추가한 소스임");
+		System.out.println("hotfix 브렌치에서 2번째 추가한 소스임");
 	}
 }
